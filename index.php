262bd871d6fc7d1614a704f736a7b843ee5c198f
arham-natiq25 <129538786+arham-natiq25@users.noreply.github.com>
2023-04-05 18:18:54 +0500

    added index.php

---
+++ index.php
@@ -0,0 +1,193 @@
+<?php include("header.php"); ?>
+  <!-- IMAGES WRAPPER  -->
+    <section id="peeps">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-3 p-0">
+                    <img src="talent/talent/abdulbz.jpg" class="img-fluid resize" alt="">
+                </div>
+                <div class="col-3 p-0 ">
+                    <img src="talent/talent/abenaadaziwaamoss.jpg" class="img-fluid resize" alt="">
+                </div>
+                <div class=" col-3 p-0">
+
+                    <img src="talent/talent/jadamson98official.jpg" class="img-fluid resize" alt="">
+                </div>
+                <div class="col-3 p-0">
+
+                    <img src="talent/talent/emiiyjade.jpg" class="img-fluid resize" alt="">
+                </div>
+            </div>
+            <div class=" row">
+                <div class="col-3 p-0">
+
+                    <img src="talent/talent/apollomu3ic.jpg" class="img-fluid resize" alt="">
+                </div>
+                <div class="col-3 p-0 ">
+                    <img src="talent/talent/caranicolekington.jpg" class="img-fluid resize" alt="">
+                </div>
+                <div class="col-3 p-0">
+                    <img src="talent/talent/kurled.jpg" alt="" class="img-fluid resize">
+                </div>
+                <div class="col-3 p-0 ">
+
+                    <img src="talent/talent/smurfboy4.jpg" class="img-fluid resize" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center fw-bold pt-4 h3">
+                    Our Talent Has Been Featured In :
+                </div>
+            </div>
+            <div class="row text-center pt-4 pb-5 ">
+                <div class="col-3 offset-md-2 col-md-2">
+                    <img src="brand/brand/dailystar.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-3 col-md-2">
+                    <img src="brand/brand/papermag.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-3 col-md-2">
+                    <img src="brand/brand/thecut.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-3 col-md-2">
+                    <img src="brand/brand/vice.png" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+  <!-- MAIN CONTENT OF VIDEOS -->
+    <section id="main">
+        <div class="container">
+            <div class="row">
+                <div class="offset-2 col-8 text-center py-3">
+                    <div class="display-5 fw-bolder gogfont ">
+                        ANYSTREAMERS TALENT REPRESENTS <br> PIONEERING CREATORS THAT ARE SHAPING THE <br> FUTURE OF
+                        ENTERTAINMENT
+                    </div>
+                    <p class="small">
+                        What carries us is the work of respring to build carrier .Lets start and inspire other
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container my-5">
+            <!-- HERE VIDEOS AND PICS THAT LEFT WOULD BE ADDED -->
+            <div class="row">
+                <div class="col-md-4">
+                    <video src="vdo1.mp4"  loop autoplay muted type="video/mp4" height="100%" width="100%"
+                        class="p-sm-0 p-5"></video>
+                </div>
+                <div class="col-md-4">
+                    <div class="row p-sm-0 p-5">
+                        <div class="col-12">
+                            <img src="pic1.jpeg" class="img-fluid " alt="">
+                        </div>
+                        <div class="col-12 pt-3">
+                            <img src="pic2.jpeg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <video src="vdo2.mp4"  loop autoplay muted type="video/mp4" height="100%" width="100%"
+                        class="p-sm-0 p-5"></video>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="views" class="blk">
+        <div class="container-fluid bg-dark">
+            <div class="container py-4">
+                <div class="row">
+                    <div class="col-4 text-center">
+                        <div class="display-4 fw-bolder text-warning">
+                            23.6M<sup>+</sup>
+                        </div>
+                        <br>
+                        <p class="small text-white orange">
+                            TickTock Followers
+                        </p>
+                    </div>
+                    <div class="col-4 text-center">
+                        <div class="display-4 fw-bolder text-warning">
+                            71<sup>+</sup>
+                        </div>
+                        <br>
+                        <p class="small text-white">
+                            Crossers
+                        </p>
+                    </div>
+                    <div class="col-4 text-center">
+                        <div class="display-4 fw-bolder text-warning">
+                            17M<sup>+</sup>
+                        </div>
+                        <br>
+                        <p class="small text-white">
+                            Instagram Followers
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="logs my-5">
+        <div class="container my-5">
+            <div class="row py-2">
+                <div class=" col-2  p-sm-0 d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/aebs.png" alt="" class="img-fluid  p-sm-3">
+                </div>
+                <div class="col-2  p-sm-0 d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/alipay.png" alt="" class="img-fluid p-sm-3">
+                </div>
+                <div class="col-2  p-sm-0 d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/China_Unicom.png" alt="" class="img-fluid  p-sm-3">
+                </div>
+                <div class="col-2  p-sm-0 d-flex  justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/chope.png" alt="" class="img-fluid p-sm-3">
+                </div>
+                <div class="col-2  p-sm-0 d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/client-logo.jpg" alt="" class="img-fluid p-sm-3">
+                </div>
+            </div>
+            <div class="row py-2">
+                <div class=" col-2  p-sm-0 d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/fish.png" alt="" class="img-fluid  p-sm-3">
+                </div>
+                <div class="col-2  p-sm-0 d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/ClubMed.png" alt="" class="img-fluid p-sm-3">
+                </div>
+                <div class="col-2  p-sm-0 d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/parkway health.png" alt="" class="img-fluid  p-sm-3">
+                </div>
+                <div class="col-2  p-sm-0 d-flex  justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/daimler.png" alt="" class="img-fluid p-sm-3">
+                </div>
+                <div class="col-2  p-sm-0 d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/ziroom.png" alt="" class="img-fluid p-sm-3">
+                </div>
+            </div>
+            <div class="row py-2">
+                <div class=" col-2  d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/ecnu.jpg" alt="" class="img-fluid  p-sm-3">
+                </div>
+                <div class="col-2  d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/nord.png" alt="" class="img-fluid p-sm-3">
+                </div>
+                <div class="col-2  d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/Inditex.png" alt="" class="img-fluid  p-sm-3">
+                </div>
+                <div class="col-2  d-flex  justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/evisu.png" alt="" class="img-fluid p-sm-3">
+                </div>
+                <div class="col-2  d-flex justify-content-center align-items-center mx-auto">
+                    <img src="brand/brand/ceibs.png" alt="" class="img-fluid p-sm-3">
+                </div>
+            </div>
+        </div>
+    </section>
+<?php
+include "footer.php";
+?>
